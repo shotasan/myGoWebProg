8ea067e9f7ec37a9e65abb749530d8826c667f6b
shotasan <wondermoai@gmail.com>
2020-04-15 21:52:17 +0900

    [add]マルチプレクサ・ハンドラ

--- main.go
+++ main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
+	// マルチプレクサの生成
 	mux := http.NewServeMux()
+
+	// 指定されたディレクトリからファイルを配信するハンドラ
+	files := http.FileServer(http.Dir("/public"))
+	// 作成したハンドラをマルチプレクサの関数Handleにわたす。
+	// リクエストのURLパスからプレフィックスを削除する
+	mux.Handle("/static", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
