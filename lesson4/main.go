2d270515d35b085928dd3c9a0b099b7b78bf9483
shotasan <wondermoai@gmail.com>
2020-04-18 20:41:12 +0900

    4.6

--- lesson4/main.go
+++ lesson4/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Fprintln(w, r.Form)
+	r.ParseMultipartForm(1024)
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	file, err := fileHeader.Open()
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
 }
 
 func main() {
