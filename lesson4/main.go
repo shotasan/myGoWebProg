b8c6f920be2c26592fb4e6dff1c41cdbb57ed8ee
shotasan <wondermoai@gmail.com>
2020-04-18 22:06:20 +0900

    4.11

--- lesson4/main.go
+++ lesson4/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Post struct {
+	User    string
+	Threads []string
+}
 
 func writeExample(w http.ResponseWriter, r *http.Request) {
 	str := `<html>
@@ -23,6 +29,16 @@ func headerExample(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(302)
 }
 
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "Sau Sheong",
+		Threads: []string{"一番目", "二番目", "三番目"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
@@ -30,5 +46,6 @@ func main() {
 	http.HandleFunc("/write", writeExample)
 	http.HandleFunc("/writeheader", writeHeaderExample)
 	http.HandleFunc("/redirect", headerExample)
+	http.HandleFunc("/json", jsonExample)
 	server.ListenAndServe()
 }
