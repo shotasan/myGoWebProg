e895ec21e206abd7c46b568161377db75b2d71b8
shotasan <wondermoai@gmail.com>
2020-04-18 13:49:19 +0900

    4.2

---
+++ lesson4/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func headers(w http.ResponseWriter, r *http.Request) {
+	// リクエストのヘッダを取得
+	h := r.Header
+	fmt.Fprintln(w, h)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/headers", headers)
+	server.ListenAndServe()
+}
