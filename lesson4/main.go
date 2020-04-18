5657fdecd26cb5d06cfd1277924a9119f5a7741e
shotasan <wondermoai@gmail.com>
2020-04-18 20:54:41 +0900

    4.9

--- lesson4/main.go
+++ lesson4/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,10 +13,16 @@ func writeExample(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(str))
 }
 
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "そのようなサービスはありません。")
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
 	server.ListenAndServe()
 }
