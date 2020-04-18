05250b438e9f8fdc4f8bb20b938044decc46aa89
shotasan <wondermoai@gmail.com>
2020-04-18 20:51:31 +0900

    4.8

--- lesson4/main.go
+++ lesson4/main.go
@@ -1,27 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
-func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
-	if err == nil {
-		data, err := ioutil.ReadAll(file)
-		if err == nil {
-			fmt.Fprintln(w, string(data))
-		}
-	}
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+	<head><title>Go Web Programing</title></head>
+	<body><h1>Hello World</h1></body>
+	</html>`
+	w.Write([]byte(str))
 }
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/process", process)
+	http.HandleFunc("/write", writeExample)
 	server.ListenAndServe()
 }
