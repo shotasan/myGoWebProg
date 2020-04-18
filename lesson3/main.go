d3a7b5fc02b33045107151b4d84531d82d049141
shotasan <wondermoai@gmail.com>
2020-04-18 13:42:36 +0900

    3.10

--- lesson3/main.go
+++ lesson3/main.go
@@ -3,25 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
 )
 
-// ハンドル関数の定義
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
 }
 
-func world(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "World")
+func log(h http.HandlerFunc) http.HandlerFunc {
+	// 無名関数のHandlerFuncを返す
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called - " + name)
+		// 引数として渡されたハンドラ関数を呼び出す
+		h(w, r)
+	}
 }
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-
-	// HandleFunc関数で引数の関数をハンドラに変換する
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/world", world)
-
+	http.HandleFunc("/hello", log(hello))
 	server.ListenAndServe()
 }
