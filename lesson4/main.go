db812da04e23cde8d23c36ea72343a79cd26f6f1
shotasan <wondermoai@gmail.com>
2020-04-18 22:41:24 +0900

    4.16

--- lesson4/main.go
+++ lesson4/main.go
@@ -16,16 +16,24 @@ func setCookie(w http.ResponseWriter, r *http.Request) {
 		Value:    "Mannig Co",
 		HttpOnly: true,
 	}
-	fmt.Printf("Type: %T", c1)
+	fmt.Printf("cookie: %T\n", c1)
 	// w.Header().Set("Set-Cookie", c1.String())
 	// w.Header().Add("Set-Cookie", c2.String())
 	http.SetCookie(w, &c1)
 	http.SetCookie(w, &c2)
 }
 
-func getCookie(w http.ResponseWriter, r *http.Request) {
-	h := r.Header["Cookie"]
-	fmt.Fprintln(w, h)
+func showMessage(w http.ResponseWriter, r *http.Request) {
+	// 特定のクッキーを取得する
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookies")
+	}
+	// すべてのクッキーを取得する
+	cs := r.Cookies()
+	fmt.Printf("r cookie: %T\n", cs)
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
 }
 
 func main() {
@@ -33,6 +41,6 @@ func main() {
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/set_cookie", setCookie)
-	http.HandleFunc("/get_cookie", getCookie)
+	http.HandleFunc("/show_message", showMessage)
 	server.ListenAndServe()
 }
