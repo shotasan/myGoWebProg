c8a1f655423cdfba59b2411195a3cd73292fd239
shotasan <wondermoai@gmail.com>
2020-05-05 12:20:05 +0900

    8.4

---
+++ lesson8/parallel_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+// t.Parallelが並行実行のシグナルになる
+func TestParallel_1(t *testing.T) {
+	t.Parallel()
+	time.Sleep(1 * time.Second)
+}
+
+func TestParallel_2(t *testing.T) {
+	t.Parallel()
+	time.Sleep(2 * time.Second)
+}
+
+func TestParallel_3(t *testing.T) {
+	t.Parallel()
+	time.Sleep(3 * time.Second)
+}
